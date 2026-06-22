b69815422244238b389b951fd48b5b4aef1e16c8
Ob22a <Ob22adegefu123@gmail.com>
2026-06-22 21:19:59 +0300

    Refactor SearchAlgorithm class; separate result retrieval from run method; enhance runtime handling and maintainability

--- search/SearchAlgorithm.py
+++ search/SearchAlgorithm.py
@@ -41,22 +41,13 @@ class SearchAlgorithm(ABC):
     def depth_exceeded(self):
         return self.search_step==SearchStatus.DEPTH_EXCEEDED
     
-    def run(self,metadata=None):
+    def get_result(self, runtime=0.0, metadata=None):
         if metadata is None:
             metadata = {}
-        self.reset()
-
-        start_time = time.time()
-
-        while self.status == SearchStatus.RUNNING:
-            self.search_step()
-
-        end_time = time.time()
-
         return Result(
             success=self.status == SearchStatus.SUCCESS,
             solution=self.solution_node,
-            runtime=end_time - start_time,
+            runtime=runtime,
             nodes_expanded=getattr(self, "nodes_expanded", 0),
             nodes_generated=getattr(self, "nodes_generated", 0),
             path_cost=(
@@ -72,6 +63,18 @@ class SearchAlgorithm(ABC):
             ),
             metadata=metadata
         )
+
+    def run(self,metadata=None):
+        self.reset()
+
+        start_time = time.time()
+
+        while self.status == SearchStatus.RUNNING:
+            self.search_step()
+
+        end_time = time.time()
+
+        return self.get_result(runtime=end_time - start_time, metadata=metadata)
 
     def reset(self):
         self.frontier = []
