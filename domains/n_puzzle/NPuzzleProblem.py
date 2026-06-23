f7f634babccc297526efd184c43e6cffcf106609
Ob22a <Ob22adegefu123@gmail.com>
2026-06-23 13:47:04 +0300

    Add heuristic methods for NPuzzleProblem; implement misplaced tiles heuristic and update type hinting

--- domains/n_puzzle/NPuzzleProblem.py
+++ domains/n_puzzle/NPuzzleProblem.py
@@ -1,5 +1,6 @@
 from core.problem import SearchProblem
 from .NPuzzle import NPuzzle
+from typing import Literal
 
 class NPuzzleProblem(SearchProblem):
     def __init__(self, start_state: str, puzzle: NPuzzle, heuristic_type: str = "manhattan"):
@@ -19,10 +20,23 @@ class NPuzzleProblem(SearchProblem):
         state_list[zero_index], state_list[action] = state_list[action], state_list[zero_index]
         return "".join(state_list)
 
-    def heuristic(self, state: str) -> float:
+    def heuristic(self, state: Literal['misplaced_tile','manhattan']) -> float:
         if self.heuristic_type == "manhattan":
             return self._manhattan_distance(state)
+        
+        elif self.heuristic_type == "misplaced_tile":
+            return self._misplaced_tiles(state)
+
         return 0.0
+    
+    def _misplaced_tiles(self,state:str)->float:
+        cost = 0
+
+        for idx, c in enumerate(state):
+            if self.puzzle.goal_state[idx]!=c:
+                cost+=1
+
+        return cost
 
     def _manhattan_distance(self, state: str) -> float:
         dist = 0
