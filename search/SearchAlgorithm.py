a21f5a9fee56e1575cf7901985bff86d71b714df
Ob22a <Ob22adegefu123@gmail.com>
2026-08-01 15:37:46 +0300

    feat: added number of iteration to search algorithm class

--- search/SearchAlgorithm.py
+++ search/SearchAlgorithm.py
@@ -24,6 +24,7 @@ class SearchAlgorithm(ABC):
         self.nodes_expanded = 0
         self.nodes_generated = 0
         self.max_frontier_size = 1
+        self.num_iterations = 0
 
     @abstractmethod
     def search_step(self):
@@ -87,3 +88,4 @@ class SearchAlgorithm(ABC):
         self.nodes_expanded = 0
         self.nodes_generated = 0
         self.max_frontier_size = 1
+        self.num_iterations = 0
