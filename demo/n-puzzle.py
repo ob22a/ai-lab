99e3ffea0a3e909669742773c6ee2b7c3e0f3dbf
Ob22a <Ob22adegefu123@gmail.com>
2026-08-01 18:05:46 +0300

    feat: modified the heuristic function in n puzzle and wrote demo function to test it

---
+++ demo/n-puzzle.py
@@ -0,0 +1,57 @@
+from domains.n_puzzle.NPuzzleGenerator import NPuzzleGenerator
+from domains.n_puzzle.NPuzzle import NPuzzle
+from domains.n_puzzle.NPuzzleProblem import NPuzzleProblem
+from search.informed.IDAStar import IDAStar
+from search.informed.AStar import AStar
+from visualization.NPuzzleVisualizer import NPuzzleVisualizer
+from domains.n_puzzle.PatternDatabase import PatternDatabase
+
+import copy
+
+def main():
+    print("Initializing 8-Puzzle (3x3) GUI...")
+    size = 4
+    generator = NPuzzleGenerator(size)
+    puzzle = NPuzzle(size)
+    
+    # Generate a random state
+    start_state = generator.generate()
+    print(f"Generated Start State: {start_state}")
+
+    # pattern dbs
+    pdb_15puzzle = [PatternDatabase("./pdbs/15puzzle_12345.bin"),PatternDatabase('./pdbs/15puzzle_6789a.bin'),PatternDatabase('./pdbs/15puzzle_bcdef.bin')]
+    
+    # Setup problem and solver
+    problem = NPuzzleProblem(start_state, puzzle, heuristic_type="pattern_db", pdbs=pdb_15puzzle)
+    problem_without_gui = copy.deepcopy(problem)
+    problem_a_star = copy.deepcopy(problem)
+
+    solver = IDAStar(problem)
+
+    
+    # Setup visualizer
+    # Start with auto_run=False so user can press space or 'a' to watch it
+    # visualizer = NPuzzleVisualizer(
+    #     puzzle_size=size,
+    #     solver=solver,
+    #     window_size=600,
+    #     fps=60,
+    #     auto_run=False,
+    #     show_search_process=False
+    # )
+    
+    # print("Launching Pygame Window! Press 'A' to auto-run the search.")
+    # visualizer.run()
+
+    print("Running IDA* without GUI for performance measurement...")
+    solver_without_gui = IDAStar(problem_without_gui)
+    result = solver_without_gui.run()
+    print(result)
+    
+    print("Running A* without GUI for performance measurement...")
+    solver_a_star = AStar(problem_a_star)
+    result_a_star = solver_a_star.run()
+    print(result_a_star)
+
+if __name__ == "__main__":
+    main()
--- domains/n_puzzle/NPuzzleProblem.py
+++ domains/n_puzzle/NPuzzleProblem.py
@@ -1,14 +1,25 @@
 from core.problem import SearchProblem
 from .NPuzzle import NPuzzle
-from typing import Literal
+from .PatternDatabase import PatternDatabase
+from domains.n_puzzle.PatternDatabase import combined_pdb_heuristic
+from typing import List, Union, Literal
 
 class NPuzzleProblem(SearchProblem):
-    def __init__(self, start_state: str, puzzle: NPuzzle, heuristic_type: str = "manhattan"):
+    def __init__(self, start_state: str, puzzle: NPuzzle, heuristic_type: Literal["manhattan", "pattern_db", "misplaced_tile"] = "manhattan", pdbs: List[Union[PatternDatabase]] = None):
         super().__init__(start_state, puzzle.goal_state)
         self.puzzle = puzzle
         self.heuristic_type = heuristic_type
+        
+        self.loaded_pdbs = []
+        if pdbs:
+            print("Loading Pattern Databases for heuristic...")
+            for pdb in pdbs:
+                print(f"Loading PDB: {pdb.filename}...")
+                self.loaded_pdbs.append(pdb)
+            
+            print(f"Loaded {len(self.loaded_pdbs)} Pattern Databases for heuristic.")
 
-    def get_actions(self, state: str) -> list:
+    def get_actions(self, state: str) -> list[int]:
         # The action is simply the index we are going to swap the '0' tile with!
         zero_index = state.index('0')
         return self.puzzle.moves_map[zero_index]
@@ -20,23 +31,19 @@ class NPuzzleProblem(SearchProblem):
         state_list[zero_index], state_list[action] = state_list[action], state_list[zero_index]
         return "".join(state_list)
 
-    def heuristic(self, state: Literal['misplaced_tile','manhattan']) -> float:
+    def heuristic(self, state: str) -> float:
         if self.heuristic_type == "manhattan":
             return self._manhattan_distance(state)
-        
+        elif self.heuristic_type == "pattern_db":
+            return self._pdb_heuristic(state)
         elif self.heuristic_type == "misplaced_tile":
-            return self._misplaced_tiles(state)
-
+            return float(sum(1 for i, char in enumerate(state) if char != '0' and char != self.puzzle.goal_state[i]))
         return 0.0
-    
-    def _misplaced_tiles(self,state:str)->float:
-        cost = 0
-
-        for idx, c in enumerate(state):
-            if self.puzzle.goal_state[idx]!=c:
-                cost+=1
-
-        return cost
+    def _pdb_heuristic(self, state: str) -> float:
+        if not self.loaded_pdbs:
+            return 0.0
+            
+        return float(combined_pdb_heuristic(state, self.loaded_pdbs))
 
     def _manhattan_distance(self, state: str) -> float:
         dist = 0
